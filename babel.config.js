f9633a4cbd5f3b5ed3895a0d820d21d694339ce3
Jasper Bernales <bernalesjasper@gmail.com>
2019-02-17 12:42:52 +0800

    build changes

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react'
+  ],
+  plugins: [
+    "@babel/plugin-proposal-class-properties",
+    'react-hot-loader/babel'
+  ]
+}
