76677e1a5f3709fb1d5592c52643c1990e466122
Jojo Tutor <jeshrave003@gmail.com>
2019-02-20 16:14:36 +0800

    page not found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Notfound() {
-  return(
+const Notfound = () => (
+  <div>
+    Page Not Found
     <div>
-      Page Not Found
-      <div>
-        <Link to='/'>Home</Link>
-      </div>
+      <Link to='/'>Back to Home</Link>
     </div>
-  )
-}
+  </div>
+)
+
+export default Notfound
